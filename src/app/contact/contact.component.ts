926cb9ada3e901bbef5154ae9d3a3b8e16edbb87
FarasatN <ferasetnovruzov1@gmail.com>
2023-02-21 19:16:12 +0400

    test

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -88,7 +88,7 @@ export class ContactComponent implements OnInit {
     setTimeout(()=>{
       this.isSubmit=false;
       window.location.reload();
-    },7000);
+    },6000);
       // this.formGroup.reset()
 
     
